99132f6773e99627b1368d3bb97195587efaa072
stream1080 <fmxhax638@foxmail.com>
2021-12-26 20:32:20 +0800

    feat: 添加获取商品列表接口

--- src/main/java/com/stream/payment/controller/ProductController.java
+++ src/main/java/com/stream/payment/controller/ProductController.java
@@ -1,9 +1,18 @@
 package com.stream.payment.controller;
 
 
+import com.stream.payment.entity.Product;
+import com.stream.payment.service.ProductService;
+import com.stream.payment.vo.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +23,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-12-26
  */
 @RestController
-@RequestMapping("/product")
+@Api(tags = "商品管理")
+@RequestMapping("/api/product")
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @ApiOperation("商品列表")
+    @GetMapping("/list")
+    public R list(){
+
+        List<Product> list = productService.list(null);
+        return R.ok().data("productList", list);
+    }
 
 }
 
